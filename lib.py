dd920303ff29bb89956fa06a1724e74d3ece8b3e
Aleksandr Chuklin <a.chuklin@gmail.com>
2018-09-14 23:42:33 +0200

    lib.py skeleton

---
+++ lib.py
@@ -0,0 +1,10 @@
+#  Collection of library functions
+
+import numpy as np
+
+VECTOR_DIMENTIONALITY = 300
+
+def document_to_vector(document):
+    # TODO: implement
+    return np.zeros(VECTOR_DIMENTIONALITY)
+
