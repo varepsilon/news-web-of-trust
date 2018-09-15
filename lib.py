a7a35556c02a3bd0eef1066bf733a7bc8699ef84
Aleksandr Chuklin <a.chuklin@gmail.com>
2018-09-15 02:30:35 +0200

    More tests/debugging

--- lib.py
+++ lib.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python
-
 #  Collection of library functions
 
+from __future__ import print_function
+
+import sys
+
 import numpy as np
+import scipy
+
 import nltk
 import gensim
-import scipy
+
+
+DEBUG = True
+
 
 class Doc2Vec:
     def __init__(self, model_path):
@@ -15,12 +22,16 @@ class Doc2Vec:
     def get_vector(self, document):
         sum_vec = 0
         l = 0
+        oov_words = set()
         for word in nltk.tokenize.word_tokenize(document):
             try:
                 sum_vec += self.model.wv[word]
                 l += 1
             except KeyError:
+                oov_words.add(word)
                 continue
+        if DEBUG:
+            print(oov_words, file=sys.stderr)
         return sum_vec / l
 
     @staticmethod
@@ -37,6 +48,9 @@ if __name__ == '__main__':
         for j in range(i + 1, len(docs)):
             d1 = docs[i]
             d2 = docs[j]
-            print('sim([{}],[{}])= {}'.format(
+            print('sim([{}],[{}]) = {}'.format(
                     d1, d2,
                     doc2vec.sim(doc2vec.get_vector(d1), doc2vec.get_vector(d2))))
+
+    with open('./testdata/cleaned_news.txt') as test_data:
+        vec = doc2vec.get_vector(test_data.read())
